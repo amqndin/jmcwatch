c063550e0687eaa67c46a2c8019f089a8239a9d0
amandin <123622161+amawdiin@users.noreply.github.com>
2023-12-18 20:38:29 +0600

    1.5.0 supposedly fixed linux issue

---
+++ upload.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rd /S /Q "./build" >nul
+rd /S /Q "./dist" >nul
+rd /S /Q "./jmcwatch.egg-info" >nul
+
+echo Press Enter to insert the saved PyPI token.
+echo Press any other key to insert it manually.
+set /p input=
+
+if not defined input (
+    for /f "tokens=1 delims=" %%a in (../pypi_token.txt) do set pypi_token=%%a
+) else (
+    set /p pypi_token=Enter PyPI token manually:
+)
+
+python setup.py sdist
+twine upload dist/* -u __token__ -p %pypi_token%
+echo twine upload dist/* -u __token__ -p %pypi_token%
