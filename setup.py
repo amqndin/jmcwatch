ac3d609fbbf26a55864527913d657ce70476f343
amandin <123622161+amawdiin@users.noreply.github.com>
2023-12-19 00:52:04 +0600

    b

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmcwatch',
-    version='1.5.4',
+    version='1.5.6',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'jmcwatch=jmcwatch.cli:main',
         ],
     },
-    install_requires=[
-        'click',
-        'watchdog',
-        'jmcfunction'
-    ],
-    dependency_links=[
-        'git+https://github.com/WingedSeal/jmc.git@src#egg=jmc'
-    ],
+    # install_requires=[
+    #     'click',
+    #     'watchdog',
+    #     'jmcfunction'
+    # ],
     author='amandin',
     author_email='amawwdin@email.com',
     description='Watch and compile JMC files. Simply type `jmcwatch` to use.',
